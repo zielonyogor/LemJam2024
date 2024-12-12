09402c22e88bbf4099ee67e479b23acbe1a79226
Kasia Jaromirska <kasia.jaromirska@gmail.com>
2024-12-12 21:44:03 +0100

    Restricting movement

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -5,31 +5,40 @@ public class PlayerInput : MonoBehaviour
 {
     public int id = 1;
 
-    public int gridWidth = 4;
+    private Vector2Int currentIndex = new Vector2Int(0, 0);
 
-
-    void Start()
+    private void Start()
     {
-
+        Vector2 newPosition = GridManager.Instance.GetGridPosition(currentIndex, id);
+        transform.position = newPosition;
     }
-
 
     public void OnPlayerMove(InputAction.CallbackContext context)
     {
         if (!context.performed) return;
 
         Vector2 moveVector = context.ReadValue<Vector2>();
-        Vector3Int positionVector = new Vector3Int((int)moveVector.x, (int)moveVector.y, 0);
-        transform.position += positionVector * gridWidth;
 
-        if (id == 1)
-        {
-            Debug.Log("plaeyr 1 move");
-        }
-        else
-        {
-            Debug.Log("Player 2 move");
-        }
+        currentIndex += new Vector2Int((int)moveVector.x, (int)moveVector.y);
+
+        if (currentIndex.x >= 3) { currentIndex.x = 2; }
+        if (currentIndex.x < 0) currentIndex.x = 0;
+        if (currentIndex.y <= -3) currentIndex.y = -2;
+        if (currentIndex.y > 0) currentIndex.y = 0;
+
+        Debug.Log("Current player Local Position: " + currentIndex);
+
+        Vector2 newPosition = GridManager.Instance.GetGridPosition(currentIndex, id);
+        transform.position = newPosition;
+
+        // if (id == 1)
+        // {
+        //     Debug.Log("plaeyr 1 move");
+        // }
+        // else
+        // {
+        //     Debug.Log("Player 2 move");
+        // }
     }
 
 }
