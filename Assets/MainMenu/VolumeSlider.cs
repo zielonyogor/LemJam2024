bde9ca32b7a0d9943d1d47816a0b46835e425b76
Lin1ux <liniuxpl2@gmail.com>
2024-12-12 22:50:01 +0100

    How to play UI

--- Assets/MainMenu/VolumeSlider.cs
+++ Assets/MainMenu/VolumeSlider.cs
@@ -4,13 +4,15 @@ using UnityEngine.UI;
 public class VolumeSlider : MonoBehaviour
 {
     [SerializeField] private Slider VolumeSliderObject;
+    [SerializeField] private float StartValue = 0;
 
 
     void Start()
     {
         if(!PlayerPrefs.HasKey("musicVolume"))
         {
-            PlayerPrefs.SetFloat("musicVolume",1);
+            PlayerPrefs.SetFloat("musicVolume",StartValue);
+            ChangeVolume();
             Load();
         }
         else
@@ -29,10 +31,12 @@ public class VolumeSlider : MonoBehaviour
     private void Load()
     {
         VolumeSliderObject.value = PlayerPrefs.GetFloat("musicVolume");
+        AudioListener.volume = VolumeSliderObject.value;
     }
 
     private void Save()
     {
         PlayerPrefs.SetFloat("musicVolume",VolumeSliderObject.value);
     }
+
 }
