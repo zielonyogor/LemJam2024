b39d9d8519af78c4dad43395a7c8354930a4d6f6
Lin1ux <liniuxpl2@gmail.com>
2024-12-13 01:06:51 +0100

    pause menu switch

--- Assets/Interface/PauseMenu/MenuPause.cs
+++ Assets/Interface/PauseMenu/MenuPause.cs
@@ -1,16 +1,42 @@
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class MenuPause : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private bool Pause = false;
+    public static UnityEvent PauseOn = new UnityEvent(); 
+    public static UnityEvent PauseOff = new UnityEvent(); 
+    
+    public void ReturnToGame()
     {
-        
+        Pause = true;
+        transform.GetChild(0).gameObject.SetActive(false);
+        PauseOff.Invoke();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SwitchPause()
     {
-        
+        if(Pause)
+        {
+            Pause = false;
+            transform.GetChild(0).gameObject.SetActive(true);
+            PauseOn.Invoke();
+            return;
+        }
+        ReturnToGame();
     }
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void OnExit(InputAction.CallbackContext context)
+    {
+        if(!context.performed) return;
+        SwitchPause();
+    }
+
 }
